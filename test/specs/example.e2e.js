465bb2b7903fb4cf33ebcc5f4ea387a200ecccef
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-01 11:30:57 +0530

    Fixed navigation to login screen

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,11 +1,12 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+const LoginInfo = require('../resources/login.info')
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await LoginPage.login(LoginInfo.email, LoginInfo.password);
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
