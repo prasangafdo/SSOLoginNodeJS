3c97c4f34105f382fc0373f43a2c775b0cba689a
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-02 20:07:22 +0530

    Added login verifications

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,13 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const LandingPage = require('../pageobjects/landing.page');
 const LoginInfo = require('../resources/login.info')
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
-
         await LoginPage.login(LoginInfo.email, LoginInfo.password);
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        await expect(LandingPage.lblUserEmail).toBeExisting();
+        await expect(LandingPage.lblUserEmail).toHaveTextContaining(LoginInfo.email);
     });
 });
 
