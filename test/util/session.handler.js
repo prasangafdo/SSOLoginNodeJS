05a505f7e3d28d62a60eeb0a56998ef9b1e4e831
Prasanga Fernando <prasangafdz@outlook.com>
2022-08-09 16:56:23 +0530

    Minor changes

--- test/util/session.handler.js
+++ test/util/session.handler.js
@@ -13,43 +13,44 @@ class sessionHandler{
             await browser.pause(4000)
             url = await browser.getUrl()
         }
-        // console.log(await browser.getCookies())
+        let allCookies = await browser.getCookies()
+        console.log('allCookies',allCookies)
         // console.log('ESTSAUTHLIGHT',await browser.getCookies['ESTSAUTHLIGHT'])
         // console.log('ESTSAUTHPRESISTENT',await browser.getCookies(['ESTSAUTHPRESISTENT']))
         // console.log('ESTSAUTH',await browser.getCookies(['ESTSAUTH']))
         // console.log('SSOCOOKIEPULLED',await browser.getCookies(['SSOCOOKIEPULLED']))
 
         // let cookie1 = await browser.getCookies['ESTSAUTHLIGHT']
-        let cookie2 = await browser.getCookies(['ESTSAUTHPRESISTENT'])
-        let cookie3 = await browser.getCookies(['ESTSAUTH'])
-        let cookie4 = await browser.getCookies(['SSOCOOKIEPULLED'])
+        let cookie2 = await browser.getCookies(['__Host-MSAAUTH'])
+        // let cookie3 = await browser.getCookies(['ESTSAUTH'])
+        // let cookie4 = await browser.getCookies(['SSOCOOKIEPULLED'])
 
         console.log(cookie2[0])
-        console.log(cookie3[0])
-        console.log(cookie4[0])
+        // console.log(cookie3[0])
+        // console.log(cookie4[0])
 
         // this.value1 = cookie1[0]?.value;
         this.value2 = cookie2[0].value;
-        this.value3 = cookie3[0].value;
-        this.value4 = cookie4[0].value;
+        // this.value3 = cookie3[0].value;
+        // this.value4 = cookie4[0].value;
     }
 
     async saveSessionToExcelSheet(){
 
         // console.log('ESTSAUTHLIGHT',this.value1)
-        console.log('ESTSAUTHPRESISTENT',this.value2)
-        console.log('ESTSAUTH',this.value3)
-        console.log('SSOCOOKIEPULLED',this.value4)
-//         let fileLocation = './test/testdata/TestDataWrite.xlsx'
-//
-//         const file = XLSX.readFile(fileLocation)
-//
-//         const ws = XLSX.utils.json_to_sheet(data)
-//
-//         XLSX.utils.book_append_sheet(file,ws,"Car")
-//
-// // Writing to our file
-//         XLSX.writeFile(file,fileLocation)
+        console.log('__Host-MSAAUTH',this.value2)
+        // console.log('ESTSAUTH',this.value3)
+        // console.log('SSOCOOKIEPULLED',this.value4)
+        let fileLocation = './test/testdata/TestDataWrite.xlsx'
+
+        const file = XLSX.readFile(fileLocation)
+
+        const ws = XLSX.utils.json_to_sheet(data)
+
+        XLSX.utils.book_append_sheet(file,ws,"Car")
+
+// Writing to our file
+        XLSX.writeFile(file,fileLocation)
     }
 
 }
